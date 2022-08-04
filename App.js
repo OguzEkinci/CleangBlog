72b55e824ccdb585c56c8a6ce25589f41c220fa3
OguzEkinci <by.engineer16@gmail.com>
2022-08-04 15:50:51 +0300

    pagination

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import express from 'express'
+import ejs from 'ejs'
 
 const app = express()
 const port = 3000
 const blog = { id: 1, title: "Blog title", description: "Blog description" }
 
+//TEMPLATE ENGINE
+app.set("view engine", "ejs")
+
+//MIDDLEWARES
+app.use(express.static('public'))
+
+//ROUTES
 app.get('/',(req,res) => {
-    res.send(blog)
+    res.render('index')
+})
+
+app.get('/about',(req,res) => {
+    res.render('about')
+})
+
+app.get('/add_post',(req,res) => {
+    res.render('add_post')
 })
 
 app.listen(port, () => {
